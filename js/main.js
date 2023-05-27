be4064150ba0edb585db9b9c7b2f55e970d5394a
YevhenHryhorenko <116802765+YevhenHryhorenko@users.noreply.github.com>
2023-05-27 10:50:58 +0300

    start this shit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const tournamentName = ""
+const jury = {
+    jury1: {
+        name: "",
+        role: ""
+    }
+}
+
+const participants = {
+    participant: {
+        name: "",
+        id: ""
+    }
+}
+
+class Jury {
+    constructor(name, role) {
+
+    }
+}
+
+class Participant {
+    constructor(name, id) {
+
+    }
+}
